c1d57c35947058ca8afaf3f48d8bbff747ba298b
DanilaYazykov <danila_yazykov@mail.ru>
2022-11-23 21:07:09 +0300

    Sprint8

---
+++ app/src/main/java/com/example/playlistmaker55555/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.playlistmaker55555
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val buttonSearch: Button = findViewById(R.id.search)
+        buttonSearch.setOnClickListener {
+            val intent = Intent(this, SearchingActivity::class.java)
+            startActivity(intent)
+        }
+
+        val buttonMedia = findViewById<Button>(R.id.media)
+        val buttonMediaSetOnClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(this@MainActivity, MediaActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        buttonMedia.setOnClickListener(buttonMediaSetOnClickListener)
+
+        val buttonSettings = findViewById<Button>(R.id.setting)
+        buttonSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/playlistmaker55555/SettingsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.playlistmaker55555
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
+
+
+//val intent = Intent(this, SettingsActivity::class.java)
+//startActivity(intent)
